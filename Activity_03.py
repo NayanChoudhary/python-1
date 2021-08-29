db5a6d5e20fc578d065b9efcd7d30f44a9fe1110
Nayan Choudhary <89688414+NayanChoudhary@users.noreply.github.com>
2021-08-29 20:38:33 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -1,4 +1,2 @@
->>> print ("NayanChoudhary")
-NayanChoudhary
->>> print ("NayanChoudhary" * 5)
-NayanChoudharyNayanChoudharyNayanChoudharyNayanChoudharyNayanChoudhary
+print ("NayanChoudhary")
+print ("NayanChoudhary" * 5)
