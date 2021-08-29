8a808db0f571707c1491fa38868cc4d5449cd6b7
Nayan Choudhary <89688414+NayanChoudhary@users.noreply.github.com>
2021-08-29 21:54:08 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -1,5 +1,8 @@
 s1=input()
 s2=input()
 print(s1,s2,sep=" ")
-for _ in range(5):
-    print(s1,s2,sep=" ")
+print(s1,s2,sep=" ")
+print(s1,s2,sep=" ")
+print(s1,s2,sep=" ")
+print(s1,s2,sep=" ")
+print(s1,s2,sep=" ")
